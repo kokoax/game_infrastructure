2edea5504a0e26ea3eb7094dc7609db8b3a5039e
kokoax <yomo4808@gmail.com>
2020-05-18 22:16:07 +0900

    First commit

---
+++ orchestration/7dtd/ap-northeast-1/spot_instance.tf
@@ -0,0 +1,48 @@
+data aws_ssm_parameter amzn2_ami {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_key_pair" "access_7dtd_instance" {
+  key_name   = "7dtd-instance-access-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCnZ0XLXWVjFcQ90FHlDpMt3Z99OPEBt290Y5aA9tBA/OxL7yxo4ORLwh0tMNQ2CrQ8mriTd7+y85WiwCmKGxxVdWlSEVWtT7SCjlKrCUXnzZqv1PWJXlx5TZ5IbJ5SFDUp90Tv2SnuWopdOYRWD2gzOj7Tj8k39ivff3W3z7bUFfOhba0uTEhKxlvgrbRxRLBNYJuNErrvh4TAtYe3b0+CKAnbX4PAxMzPH9g6c2DRQbSsUTbmF4KwzdBxCng3wRRIFaB985nu3QP6W8Jzv1smuD2MAySbKP5YW5wEGgcfMk9o2p7TLfFeDgmjvO4ScnHNj9pQJXftuLeLIIRKXkB5egZ69FaLA3gtPXyCHzqJW2/96tGWnaChwwD8aCA4Uqco+KPFoXQZD4jQEHafxjgf5iJ9D2NPT8vppAwBu5lppWWZVx2LljxS1MLiHEWneusCcDoVoNs5ZfN83fIRB03YYyl7QngsWQO9hdpLg6s/rNWP8huGobzENDzPv3ao6dM= y-tokoi@y-tokoi.voyagegroup.local"
+}
+
+resource "aws_spot_instance_request" "_7dtd_spot_instance_request" {
+  ami           = data.aws_ssm_parameter.amzn2_ami.value
+  instance_type = "c5.large"
+  subnet_id     = aws_subnet.public.id
+  spot_price = '0.1'
+  security_groups = [aws_security_group._7dtd_security_group.id]
+
+  tags = {
+    Name = "7dtd-spot-instance"
+  }
+}
+
+resource "aws_security_group" "_7dtd_security_group" {
+  name        = "7dtd_security_group"
+  description = "for instance ssh"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 25565
+    to_port = 25565
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
