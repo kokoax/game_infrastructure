4e582f4ef684f89ab4bd93466472231eabcddd15
kokoax <yomo4808@gmail.com>
2020-05-24 22:33:14 +0900

    Add notice ec2 terminated

--- orchestration/7dtd/ap-northeast-1/lambda.tf
+++ orchestration/7dtd/ap-northeast-1/lambda.tf
@@ -81,3 +81,22 @@ resource "aws_lambda_function" "notice_public_ip_to_slack_lambda" {
 
   runtime = "python3.7"
 }
+
+resource "aws_lambda_function" "notice_terminate_to_slack_lambda" {
+  filename      = "lambda/notice_terminate_to_slack_lambda/lambda_handler.zip"
+  function_name = "notice_terminate_to_slack_lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "notice_terminate_to_slack_lambda.lambda_handler"
+  timeout       = 60
+
+  source_code_hash = filebase64sha256("lambda/notice_terminate_to_slack_lambda/lambda_handler.zip")
+  kms_key_arn      = "arn:aws:kms:ap-northeast-1:886398035123:key/097a43e9-d69e-4923-b358-031210b01d09"
+
+  environment {
+    variables = {
+      ENCRYPTED_SLACK_WEBHOOK = "AQICAHiRp81+n7J5EkKb8abQ1TUOTvEcUtk8qRwF3ViprAViXAHjq6O2hHiuYEc4+YyLfwC6AAAAsjCBrwYJKoZIhvcNAQcGoIGhMIGeAgEAMIGYBgkqhkiG9w0BBwEwHgYJYIZIAWUDBAEuMBEEDCrh7ebsnU1yOPIV8AIBEIBrJDLTWWW3/NyLQdIKtqIbcnYcWnBvRC9Rr/gGas8vTAbCOhnmmS3Y/9uyEcPUfiw95BRW00NbgqkhMuU6zQKXv3d+budc2/g52bv+R3klgmaOT70b3VqfqfMZ4gFqzEMk1Toxr1MnqnpgNDQ="
+    }
+  }
+
+  runtime = "python3.7"
+}
